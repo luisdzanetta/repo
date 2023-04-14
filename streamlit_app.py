946cf81acb6a4a8413dfbff670ba6096b59e7481
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-14 17:38:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
     ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941')
     ax1.set_xlabel('Week')
     ax1.set_ylabel('MDE (%)', color='#ED1941')
-    ax1.tick_params('y', colors='#ED1941')
+    #ax1.tick_params('y', colors='#ED1941')
     ax1.set_title("Minimum Detectable Effect vs. Total Sample Size by week", fontsize=6, fontweight='bold', pad=10)
     ax1.set_xlabel('Week', fontsize=6, labelpad=8)
     ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=6, color='#ED1941', labelpad=10)
@@ -51,7 +51,7 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
           
     ax2 = ax1.twinx()
     ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=0.6, color='#000000')
-    ax2.tick_params('y', colors='#000000')
+    #ax2.tick_params('y', colors='#000000')
     ax2.set_ylabel('Total Sample Size', fontsize=6, color='#000000', labelpad=10)
     #ax2.tick_params(axis='y', colors='#000000', labelsize=5)
     ax2.yaxis.set_major_locator(MaxNLocator(nbins=5))
