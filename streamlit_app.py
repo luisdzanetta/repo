e93c3011c747b8fa79b44bf6d8979543b9b586a4
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-14 11:10:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,16 +39,18 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
     ax1.set_title("Minimum Detectable Effect vs. Total Sample Size by week", fontsize=6, fontweight='bold', pad=10)
     ax1.set_xlabel('Week', fontsize=6, labelpad=8)
     ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=6, color='#ED1941', labelpad=10)
-    ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941', linewidth=3, zorder=2)
+    ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941', linewidth=3)
     ax1.tick_params(axis='y', colors='#ED1941', labelsize=6)
     ax1.tick_params(axis='x', labelsize=6)
     ax1.grid(alpha=0.3)
     
     ax2 = ax1.twinx()
-    ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=1.0, width = 1, color='#000000', zorder=1)
+    ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=1.0, width = 1, color='#000000')
     ax2.tick_params('y', colors='#000000')
     ax2.set_ylabel('Total Sample Size (x1000)', fontsize=6, color='#000000', labelpad=10)
     ax2.tick_params(axis='y', colors='#000000', labelsize=6)
+    ax2.yaxis.set_major_locator(tick.LinearLocator(primary_ticks))
+    plot.set_zorder(ax2.get_zorder()+1)
 
     plt.tight_layout()
 
