d207cf74271d0fd673af919c14ad38288bda5288
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-14 14:48:46 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants):
     mde = (z_alpha + z_beta) * se / pooled_prob
     return mde
 
-session_state = st.session_state.get()
+session_state = st.session_state.get(table_df)
 
 def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_size, num_variants):
     table = []
