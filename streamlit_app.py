c60ddd8f1f32fc8a66fb13783ad736ed32cda5b0
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-14 09:51:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import seaborn as sns
 import pandas as pd
 from math import sqrt
 from scipy.stats import norm
-
 def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants):
     pooled_prob = (control_cr + cr * num_variants) / (num_variants + 1)
     se = sqrt(pooled_prob * (1 - pooled_prob) * (1/sample_size + num_variants/sample_size))
@@ -13,7 +12,6 @@ def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants):
     z_beta = abs(norm.ppf(beta))
     mde = (z_alpha + z_beta) * se / pooled_prob
     return mde
-
 def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_size, num_variants):
     table = []
     mde_values = []
@@ -28,38 +26,54 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
         table.append([week, week_sample_size_str, week_total_sample_size_str, mde_str])
         mde_values.append(mde)
         total_sample_size_values.append(week_total_sample_size)
+    sns.set_style("white")
+    sns.color_palette("rocket")
+    fig, ax1 = plt.subplots()
+    fig, ax1 = plt.subplots()
+    ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941')
+    ax1.set_xlabel('Week')
+    ax1.set_ylabel('MDE (%)', color='#ED1941')
+    ax1.tick_params('y', colors='#ED1941')
+    ax1.set_title("Minimum Detectable Effect vs. Total Sample Size", fontsize=14, fontweight='bold', pad=20)
+    ax1.set_xlabel('Week', fontsize=12, fontweight='bold', labelpad=10)
+    ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=12, fontweight='bold', color='#ED1941', labelpad=10)
+    ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941', linewidth=2)
+    ax1.tick_params(axis='y', colors='#ED1941', labelsize=10)
+    ax1.tick_params(axis='x', labelsize=10)
+    ax1.grid(alpha=0.3)
 
-        sns.set_style("white")
-        sns.color_palette("rocket")
-        fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+    ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=0.2, color='#000000')
+    ax2.set_ylabel('Total Sample Size', color='#000000')
+    ax2.tick_params('y', colors='#000000')
+    ax2.set_ylabel('Total Sample Size', fontsize=12, fontweight='bold', color='#000000', labelpad=10)
+    ax2.tick_params(axis='y', colors='#000000', labelsize=10)
 
-        # Adicione as alterações de gráfico aqui
-        ax1.set_title("Minimum Detectable Effect vs. Total Sample Size", fontsize=14, fontweight='bold', pad=20)
-        ax1.set_xlabel('Week', fontsize=12, fontweight='bold', labelpad=10)
-        ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=12, fontweight='bold', color='#ED1941', labelpad=10)
-        ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941', linewidth=2)
-        ax1.tick_params(axis='y', colors='#ED1941', labelsize=10)
-        ax1.tick_params(axis='x', labelsize=10)
-        ax1.grid(alpha=0.3)
+    plt.tight_layout()
 
-        ax2 = ax1.twinx()
-        ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=0.2, color='#000000')
-        ax2.set_ylabel('Total Sample Size', fontsize=12, fontweight='bold', color='#000000', labelpad=10)
-        ax2.tick_params(axis='y', colors='#000000', labelsize=10)
-
-        plt.tight_layout()
-        st.pyplot(fig)
-
-
+    headers = ["Week", "Sample Size per variant", "Total Sample Size", "MDE"]
+    table_df = pd.DataFrame(table, columns=headers)
+    table_styles = [
+    dict(selector="th", props=[("border", "1px solid #ccc"), ("background-color", "#f2f2f2"), ("text-align", "center"), ("padding", "8px")]),
+    dict(selector="td", props=[("border", "1px solid #ccc"), ("text-align", "center"), ("padding", "8px")]),
+    dict(selector=".row-hover:hover", props=[("background-color", "#e2e2e2")]),
+    dict(selector="table", props=[("border-collapse", "collapse"), ("box-shadow", "1px 1px 2px #ccc")])
+    ]
+    
+    table_html = table_df.style\
+        .set_table_styles(table_styles)\
+        .hide_index()\
+        .render()
+    
+    st.markdown(table_html, unsafe_allow_html=True)
+    st.pyplot(fig)
 st.title("Minimun Detectable Effect (MDE) Calculator")
 st.write("This app calculates the Minimum Detectable Effect (MDE) for conversion rate tests based on the level of statistical significance and power, number of weeks in the experiment, conversion rate of the control, sample size per week, and number of variants.")
-
 alpha = st.slider("Statistical significance (α)", 0.01, 0.1, 0.05, 0.01)
 beta = st.slider("Statistical power (β)", 0.2, 0.95, 0.8, 0.05)
 num_weeks = st.slider("Number of weeks in the experiment", 1, 20, 10, 1)
 control_cr = st.slider("Control group conversion rate (%)", 0.0, 100.0, 5.0, 0.05) / 100
 total_sample_size = st.slider("Sample size per week", 1000, 1000000, 500000, 1000)
 num_variants = st.slider("Number of variants (including control)", 1, 6, 2, 1)
-
 if st.button("Generate table and graph"):
     generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_size, num_variants)
