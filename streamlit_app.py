ca8cc777db2e9c6c539d1311b8bf71f70262d8c6
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-14 11:33:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
     sns.color_palette("rocket")
     
     fig, ax1 = plt.subplots(figsize=(4,3))
-    plt.subplots_adjust(top=0.6, bottom=0.1, left=0.1, right=0.6)
+    plt.subplots_adjust(top=0.4, bottom=0.1, left=0.1, right=0.4)
         
     ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941')
     ax1.set_xlabel('Week')
@@ -41,14 +41,14 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
     ax1.set_xlabel('Week', fontsize=6, labelpad=8)
     ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=6, color='#ED1941', labelpad=10)
     ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941', linewidth=1.5)
-    ax1.tick_params(axis='y', colors='#ED1941', labelsize=6)
-    ax1.tick_params(axis='x', labelsize=6)
+    ax1.tick_params(axis='y', colors='#ED1941', labelsize=4)
+    ax1.tick_params(axis='x', labelsize=4)
        
     ax2 = ax1.twinx()
     ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=0.6, color='#000000')
     ax2.tick_params('y', colors='#000000')
     ax2.set_ylabel('Total Sample Size (x1000)', fontsize=6, color='#000000', labelpad=10)
-    ax2.tick_params(axis='y', colors='#000000', labelsize=6)
+    ax2.tick_params(axis='y', colors='#000000', labelsize=4)
     ax1.set_zorder(ax2.get_zorder()+1)
     ax1.patch.set_visible(False)
 
