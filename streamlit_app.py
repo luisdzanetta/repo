fc30960f6a876e8d790602d10d44f96c298a0785
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-14 09:41:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,38 +29,27 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
         mde_values.append(mde)
         total_sample_size_values.append(week_total_sample_size)
 
-        # define a paleta de cores
-        colors = ['#ED1941', '#F26522', '#F7B32B', '#00A8E0', '#00CCBC']
+        sns.set_style("white")
+        sns.color_palette("rocket")
+        fig, ax1 = plt.subplots()
 
-        # cria o gráfico
-        sns.set_style("whitegrid")
-        fig, ax1 = plt.subplots(figsize=(10,6))
-
-        # define as propriedades do eixo x
+        # Adicione as alterações de gráfico aqui
+        ax1.set_title("Minimum Detectable Effect vs. Total Sample Size", fontsize=14, fontweight='bold', pad=20)
         ax1.set_xlabel('Week', fontsize=12, fontweight='bold', labelpad=10)
-        ax1.set_xticks(range(1, num_weeks+1))
+        ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=12, fontweight='bold', color='#ED1941', labelpad=10)
+        ax1.plot(range(1, num_weeks+1), mde_values, color='#ED1941', linewidth=2)
+        ax1.tick_params(axis='y', colors='#ED1941', labelsize=10)
         ax1.tick_params(axis='x', labelsize=10)
+        ax1.grid(alpha=0.3)
 
-        # define as propriedades do eixo y1
-        ax1.set_ylabel('Minimum Detectable Effect (%)', fontsize=12, fontweight='bold', color=colors[0], labelpad=10)
-        ax1.plot(range(1, num_weeks+1), mde_values, color=colors[0], linewidth=2, marker='o', markersize=8)
-        ax1.tick_params(axis='y', colors=colors[0], labelsize=10)
-
-        # define as propriedades do eixo y2
         ax2 = ax1.twinx()
-        ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=0.2, color=colors[-1])
-        ax2.set_ylabel('Total Sample Size', fontsize=12, fontweight='bold', color=colors[-1], labelpad=10)
-        ax2.tick_params(axis='y', colors=colors[-1], labelsize=10)
+        ax2.bar(range(1, num_weeks+1), total_sample_size_values, alpha=0.2, color='#000000')
+        ax2.set_ylabel('Total Sample Size', fontsize=12, fontweight='bold', color='#000000', labelpad=10)
+        ax2.tick_params(axis='y', colors='#000000', labelsize=10)
 
-        # adiciona linhas de grade horizontais para ambos os eixos
-        ax1.grid(axis='y', linestyle='--', alpha=0.7)
-        ax2.grid(axis='y', linestyle='--', alpha=0.7)
-
-        # ajusta o layout do gráfico
         plt.tight_layout()
-
-        # exibe o gráfico
         st.pyplot(fig)
+
 
 st.title("Minimun Detectable Effect (MDE) Calculator")
 st.write("This app calculates the Minimum Detectable Effect (MDE) for conversion rate tests based on the level of statistical significance and power, number of weeks in the experiment, conversion rate of the control, sample size per week, and number of variants.")
