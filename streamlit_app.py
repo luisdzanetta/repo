79e7c02f2576e339ef3e86ea2fe48d5d1fa36bb8
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-13 21:28:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_siz
 
     headers = ["Week", "Sample Size per variant", "Total Sample Size", "MDE"]
     table_df = pd.DataFrame(table, columns=headers)
-    table_df["Sample Size per variant"] = table_df["Sample Size per variant"].map('{:,.0f}'.format)
-    table_df["Total Sample Size"] = table_df["Total Sample Size"].map('{:,.0f}'.format)
+    table_df["Sample Size per variant"] = table_df["Sample Size per variant"].fillna(0).map('{:,.0f}'.format)
+    table_df["Total Sample Size"] = table_df["Total Sample Size"].fillna(0).map('{:,.0f}'.format)
     st.table(table_df)
 
     st.pyplot(fig)
