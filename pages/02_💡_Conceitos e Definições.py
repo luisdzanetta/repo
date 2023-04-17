5238c5006dec2f2228025eff1411c98aabef5f67
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 11:35:23 -0300

    Update 02_💡_Conceitos e Definições.py

--- pages/02_💡_Conceitos e Definições.py
+++ pages/02_💡_Conceitos e Definições.py
@@ -4,7 +4,7 @@ st.title("Conceitos e Definições")
 
 #Nível de significância
 st.markdown("**Nível de Significância:**")
-st.write"O nível de probabilidade em que se concorda em rejeitar a hipótese nula. Por convenção, 0.05")
+st.write("O nível de probabilidade em que se concorda em rejeitar a hipótese nula. Por convenção, 0.05")
 
 #Alfa
 st.markdown("**Alfa (α):**")
