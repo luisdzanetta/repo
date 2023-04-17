922bb7280b36bc33f84fe901e3a712b034fa6fe5
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 11:17:27 -0300

    Update 01_📊_Math and Statistics.py

--- pages/01_📊_Math and Statistics.py
+++ pages/01_📊_Math and Statistics.py
@@ -1,6 +1,3 @@
 import streamlit as st
 
 st.title("Math and Statistics")
-
-st.text_area("Nível de Significância", '''O nível de probabilidade em que se concorda em rejeitar a hipótese nula. 
-Conventionalmente estabelecido em 0,05.''')
