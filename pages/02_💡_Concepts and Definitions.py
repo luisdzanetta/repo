8344d574f72393917de2624b2f07254b2553a9d6
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 11:31:40 -0300

    Update 02_💡_Concepts and Definitions.py

--- pages/02_💡_Concepts and Definitions.py
+++ pages/02_💡_Concepts and Definitions.py
@@ -2,14 +2,22 @@ import streamlit as st
 
 st.title("Concepts & Definitions")
 
+#Nível de significância
+st.markdown("**Nível de Significância:**")
+st.markdown("O nível de probabilidade em que se concorda em rejeitar a hipótese nula. Por convenção, 0.05")
 
-st.text("Nível de Significância", '''O nível de probabilidade em que se concorda em rejeitar a hipótese nula. 
-Conventionalmente estabelecido em 0,05.''')
+#Alfa
+st.markdown("**Alfa (α):**")
+st.markdown("A probabilidade do erro tipo I.")
 
-st.text("Alfa (α):", '''A probabilidade do erro tipo I.''')
+#Erro tipo I
+st.markdown("**Erro tipo I:**")
+st.markdown("O erro que resulta quando a hipótese nula é falsamente rejeitada.")
 
-st.text("Erro tipo I", '''O erro que resulta quando a hipótese nula é falsamente rejeitada.''')
+#Erro tipo II
+st.markdown("**Erro tipo II:**")
+st.markdown("O erro que resulta quando a hipótese nula é falsamente aceita.")
 
-st.text("Erro tipo II", '''O erro que resulta quando a hipótese nula é falsamente aceita.''')
-
-st.text("Hipótese nula", '''Normalmente, a hipótese "sem diferença" ou "sem associação" a ser testada (geralmente por meio de um teste de significância) contra uma hipótese alternativa que postula diferença ou associação diferente de zero.''')
+#Hipótese nula
+st.markdown("**Hipótese nula:**")
+st.markdown("Normalmente, a hipótese "sem diferença" ou "sem associação" a ser testada (geralmente por meio de um teste de significância) contra uma hipótese alternativa que postula diferença ou associação diferente de zero.")
