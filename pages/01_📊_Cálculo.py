3b899da5f70e26c1ca85eca9932de86b720d86c4
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 14:24:56 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -4,48 +4,38 @@ import streamlit as st
 st.title("Equações utilizadas")
 
 #Equação MDE
-st.write("""
+st.markdown(r'''
 <div style="text-align: left">
-    <latex>
-    \text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}
-    </latex>
+$$\text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}$$
 </div>
-""", unsafe_allow_html=True)
+''', unsafe_allow_html=True)
 
 st.write('**Onde:**')
 
 #Equação SE
-st.write("""
+st.markdown(r'''
 <div style="text-align: left">
-    <latex>
-    \text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}
-    </latex>
+$$\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}$$
 </div>
-""", unsafe_allow_html=True)
+''', unsafe_allow_html=True)
 
 #Equação pooled prob
-st.write("""
+st.markdown(r'''
 <div style="text-align: left">
-    <latex>
-    \text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}
-    </latex>
+$$\text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}$$
 </div>
-""", unsafe_allow_html=True)
+''', unsafe_allow_html=True)
 
 #Equação z de alpha
-st.write("""
+st.markdown(r'''
 <div style="text-align: left">
-    <latex>
-    \text{z}_{\alpha/2} = |{\text{norm.ppf}}(\alpha/2)|
-    </latex>
+$$\text{z}_{\alpha/2} = |{\text{norm.ppf}}(\alpha/2)|$$
 </div>
-""", unsafe_allow_html=True)
+''', unsafe_allow_html=True)
 
 #Equação z de beta
-st.write("""
+st.markdown(r'''
 <div style="text-align: left">
-    <latex>
-    \text{z}_\beta = |{\text{norm.ppf}}(\beta)|
-    </latex>
+$$\text{z}_\beta = |{\text{norm.ppf}}(\beta)|$$
 </div>
-""", unsafe_allow_html=True)
+''', unsafe_allow_html=True)
