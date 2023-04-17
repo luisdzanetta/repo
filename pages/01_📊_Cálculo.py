8538735f42dd666ca39d07bb919236beaad5cdab
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 12:19:32 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -7,10 +7,10 @@ st.latex(r'''
 ''')
 
 st.latex(r'''
-\begin{aligned}
-SE &= \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)} \\
-\text{pooled\_prob} &= \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1} \\
-z_{\alpha/2} &= |\text{norm.ppf}(\alpha/2)| \\
-z_\beta &= |\text{norm.ppf}(\beta)|
-\end{aligned}
-''')
+\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}'''
+         
+st.latex(r'''\text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}''')
+
+st.latex(r'''\text{z_{\alpha/2}} = |\text{norm.ppf}(\alpha/2)|''')
+         
+st.latex(r'''\text{z_\beta} = |\text{norm.ppf}(\beta)|''')
