ee4f9e16b850b21cf98c438435bf56dc01547b5f
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 11:50:57 -0300

    Update 02_💡_Conceitos e Definições.py

--- pages/02_💡_Conceitos e Definições.py
+++ pages/02_💡_Conceitos e Definições.py
@@ -2,18 +2,11 @@ import streamlit as st
 
 st.title("Conceitos e Definições")
 
-
-#Nível de significância
-st.write("**Nível de Significância:** O nível de probabilidade em que se concorda em rejeitar a hipótese nula. Por convenção, 0.05.")
-
-#Poder
-st.write("**Poder:** A probabilidade de rejeitar a hipótese nula quando ela é falsa. O poder fornece um método de discriminação entre testes concorrentes da mesma hipótese, sendo preferido o teste com maior poder. É também a base dos procedimentos para estimar o tamanho da amostra necessária para detectar um efeito de uma determinada magnitude.")
-
 #Alfa
 st.write("**Alfa (α):** A probabilidade do erro tipo I.")
-         
+
 #Beta
-st.write("**Beta (β):** A probabilidade do erro tipo II.")   
+st.write("**Beta (β):** A probabilidade do erro tipo II.")
 
 #Erro tipo I
 st.write("**Erro tipo I:** O erro que resulta quando a hipótese nula é falsamente rejeitada.")
@@ -24,8 +17,16 @@ st.write("**Erro tipo II:** O erro que resulta quando a hipótese nula é falsam
 #Hipótese nula
 st.write("**Hipótese nula:** Normalmente, a hipótese 'sem diferença' ou 'sem associação' a ser testada (geralmente por meio de um teste de significância) contra uma hipótese alternativa que postula diferença ou associação diferente de zero.")
 
+#Nível de significância
+st.write("**Nível de Significância:** O nível de probabilidade em que se concorda em rejeitar a hipótese nula. Por convenção, 0.05.")
+
+#Poder
+st.write("**Poder:** A probabilidade de rejeitar a hipótese nula quando ela é falsa. O poder fornece um método de discriminação entre testes concorrentes da mesma hipótese, sendo preferido o teste com maior poder. É também a base dos procedimentos para estimar o tamanho da amostra necessária para detectar um efeito de uma determinada magnitude.")
+ 
+#Tamanho amostral
+st.write("**Tamanho amostral:** O número de indivíduos a serem incluídos em uma investigação. Normalmente escolhido para que o estudo tenha um poder particular de detectar um efeito de um tamanho particular.")
+
 #Teste de hipóteses
 st.write("**Teste de hipóteses:** Um termo geral para o procedimento de avaliar se os dados da amostra são consistentes ou não com as declarações feitas sobre a população.")
          
-#Tamanho amostral
-st.write("**Tamanho amostral:** O número de indivíduos a serem incluídos em uma investigação. Normalmente escolhido para que o estudo tenha um poder particular de detectar um efeito de um tamanho particular.")
+
