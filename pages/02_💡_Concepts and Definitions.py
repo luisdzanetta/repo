3f6c6454d38c886a5ce8d20c344aca17bc2d66a9
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 11:24:20 -0300

    Update 02_💡_Concepts and Definitions.py

--- pages/02_💡_Concepts and Definitions.py
+++ pages/02_💡_Concepts and Definitions.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.title("Concepts & Definitions")
+
+
+st.text_area("Nível de Significância", '''O nível de probabilidade em que se concorda em rejeitar a hipótese nula. 
+Conventionalmente estabelecido em 0,05.''')
+
+st.text_area("Alfa (α):", '''A probabilidade do erro tipo I.''')
+
+st.text_area("Erro tipo I", '''O erro que resulta quando a hipótese nula é falsamente rejeitada.''')
+
+st.text_area("Erro tipo II", '''O erro que resulta quando a hipótese nula é falsamente aceita.''')
+
+st.text_area("Hipótese nula", '''Normalmente, a hipótese "sem diferença" ou "sem associação"
+              a ser testada (geralmente por meio de um teste de significância)
+              contra uma hipótese alternativa que postula diferença ou associação diferente de zero.''')
