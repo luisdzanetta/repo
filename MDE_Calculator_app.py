4565e4cf71379ed5b1ba8ddfa3bce6c50faab005
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 11:02:37 -0300

    Update MDE_Calculator_app.py

--- MDE_Calculator_app.py
+++ MDE_Calculator_app.py
@@ -85,7 +85,7 @@ st.title("Calculadora de Mínimo Efeito Detectável")
 st.write("Este aplicativo calcula o Efeito Mínimo Detectável (MDE) para testes de taxa de conversão com base no nível de significância estatística e poder, número de semanas no experimento, taxa de conversão do controle, tamanho da amostra por semana e número de variantes.")
 alpha = st.slider("Alfa (α)", 0.01, 0.1, 0.05, 0.01)
 beta = st.slider("Beta (β)", 0.2, 0.95, 0.8, 0.05)
-num_weeks = st.slider("Números de semana do experimento", 1, 20, 10, 1)
+num_weeks = st.slider("Número de semanas do experimento", 1, 20, 10, 1)
 control_cr = st.slider("Conversão do grupo controle (%)", 0.0, 100.0, 5.0, 0.05) / 100
 num_variants = st.slider("Número de variantes (incluindo o controle)", 1, 6, 2, 1)
 total_sample_size = st.number_input("Amostra por semana", min_value=1, step=100, format="%i")
