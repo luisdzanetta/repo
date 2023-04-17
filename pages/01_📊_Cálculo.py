13cfc795ee420f0a52dcd79f967263c3074bcbfb
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 14:21:45 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -4,21 +4,48 @@ import streamlit as st
 st.title("Equações utilizadas")
 
 #Equação MDE
-st.latex(r'''
-\text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}
-''')
+st.write("""
+<div style="text-align: left">
+    <latex>
+    \text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}
+    </latex>
+</div>
+""", unsafe_allow_html=True)
 
 st.write('**Onde:**')
 
 #Equação SE
-st.latex(r'''
-\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}''')
+st.write("""
+<div style="text-align: left">
+    <latex>
+    \text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}
+    </latex>
+</div>
+""", unsafe_allow_html=True)
 
 #Equação pooled prob
-st.latex(r'''\text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}''')
+st.write("""
+<div style="text-align: left">
+    <latex>
+    \text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}
+    </latex>
+</div>
+""", unsafe_allow_html=True)
 
 #Equação z de alpha
-st.latex(r'''\text{z}_{\alpha/2} = |{\text{norm.ppf}}(\alpha/2)|''')
+st.write("""
+<div style="text-align: left">
+    <latex>
+    \text{z}_{\alpha/2} = |{\text{norm.ppf}}(\alpha/2)|
+    </latex>
+</div>
+""", unsafe_allow_html=True)
 
 #Equação z de beta
-st.latex(r'''\text{z}_\beta = |{\text{norm.ppf}}(\beta)|''')
+st.write("""
+<div style="text-align: left">
+    <latex>
+    \text{z}_\beta = |{\text{norm.ppf}}(\beta)|
+    </latex>
+</div>
+""", unsafe_allow_html=True)
