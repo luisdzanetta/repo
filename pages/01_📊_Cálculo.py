41a64c4fdde0d45987dfc97e568742f68c71d478
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 12:21:48 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -1,16 +1,19 @@
 import streamlit as st
 
-st.title("Cálculo utilizado")
-
+#Equação MDE
 st.latex(r'''
 \text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}
 ''')
 
+#Equação SE
 st.latex(r'''
-\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}'''
-         
+\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}''')
+
+#Equação pooled prob
 st.latex(r'''\text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}''')
 
-st.latex(r'''\text{z_{\alpha/2}} = |\text{norm.ppf}(\alpha/2)|''')
-         
-st.latex(r'''\text{z_\beta} = |\text{norm.ppf}(\beta)|''')
+#Equação z de alpha
+st.latex(r'''\text{z}_{\alpha/2} = |{\text{norm.ppf}}(\alpha/2)|''')
+
+#Equação z de beta
+st.latex(r'''\text{z}_\beta = |{\text{norm.ppf}}(\beta)|''')
