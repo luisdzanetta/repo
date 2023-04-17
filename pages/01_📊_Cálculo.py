3a6c980a673e1d107aa568265961aab2ba4f0d0b
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-17 14:42:34 -0300

    Update 01_📊_Cálculo.py

--- pages/01_📊_Cálculo.py
+++ pages/01_📊_Cálculo.py
@@ -1,16 +1,17 @@
 import streamlit as st
+from scipy.stats import norm
 
-equacao_mde = r'\text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}'
-equacao_se = r'\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}'
-equacao_pooled_prob = r'\text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}'
-equacao_z_alpha_2 = r'\text{z}_{\alpha/2} = \left|\text{norm.ppf}\left(\frac{\alpha}{2}\right)\right|'
-equacao_z_beta = r'\text{z}_\beta = \left|\text{norm.ppf}(\beta)\right|'
+st.title("Cálculo utilizado")
 
-st.title("Equações utilizadas")
-st.write('**Equação MDE:**')
-st.markdown(f'<div style="text-align: left">{equacao_mde}</div>', unsafe_allow_html=True)
-st.write('**Onde:**')
-st.markdown(f'<div style="text-align: left">{equacao_se}</div>', unsafe_allow_html=True)
-st.markdown(f'<div style="text-align: left">{equacao_pooled_prob}</div>', unsafe_allow_html=True)
-st.markdown(f'<div style="text-align: left">{equacao_z_alpha_2}</div>', unsafe_allow_html=True)
-st.markdown(f'<div style="text-align: left">{equacao_z_beta}</div>', unsafe_allow_html=True)
+st.latex(r'''
+\text{MDE} = \frac{(z_{\alpha/2} + z_\beta) \cdot SE}{\text{pooled\_prob}}
+''')
+
+st.latex(r'''
+\text{SE} = \sqrt{\text{pooled\_prob} \cdot (1 - \text{pooled\_prob}) \cdot \left(\frac{1}{\text{sample\_size}} + \frac{\text{num\_variants}}{\text{sample\_size}}\right)}''')
+
+st.latex(r'''\text{pooled\_prob} = \frac{\text{control\_cr} + \text{cr} \cdot \text{num\_variants}}{\text{num\_variants} + 1}''')
+
+st.latex(r'''\text{z}_{\alpha/2} = |{\text{norm.ppf}}(\alpha/2)|''')
+
+st.latex(r'''\text{z}_\beta = |{\text{norm.ppf}}(\beta)|''')
