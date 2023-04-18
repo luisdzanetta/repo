3bcebf6b61ef9c08d0e17b015d6fc13dc2794270
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-18 09:59:22 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -8,7 +8,7 @@ from math import sqrt
 from scipy.stats import norm
 
 def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants):
-    pooled_prob = (control_cr + cr * num_variants) / (num_variants + 1)
+    pooled_prob = (control_cr + cr * num_variants) / (num_variants)
     se = sqrt(pooled_prob * (1 - pooled_prob) * ((1 / sample_size) + (num_variants/sample_size)))
     #z_alpha = abs(norm.ppf(alpha/2))
     z_alpha = abs(norm.ppf(alpha))
