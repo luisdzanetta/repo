5a28156eaf0767d3ef5de6cbbad573f4cf1ddc11
Luis D'Avoglio Zanetta <luis.dzanetta@gmail.com>
2023-04-19 08:48:08 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -7,31 +7,60 @@ import pandas as pd
 from math import sqrt
 from scipy.stats import norm
 
-def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants):
+#Original
+#def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants):
+    #pooled_prob = (control_cr + cr * num_variants) / (num_variants)
+    #se = sqrt(pooled_prob * (1 - pooled_prob) * ((1 / sample_size) + (num_variants/sample_size)))
+    #z_alpha = abs(norm.ppf(alpha/2)) #two-tailed
+    ##z_alpha = abs(norm.ppf(alpha)) #one-tailed
+    #z_beta = abs(norm.ppf(beta))
+    #mde = (z_alpha + z_beta) * se / pooled_prob
+    #return mde
+    
+#Bonferroni     
+def calculate_mde(alpha, beta, cr, control_cr, sample_size, num_variants, num_comparisons=1):
     pooled_prob = (control_cr + cr * num_variants) / (num_variants)
     se = sqrt(pooled_prob * (1 - pooled_prob) * ((1 / sample_size) + (num_variants/sample_size)))
-    z_alpha = abs(norm.ppf(alpha/2)) #two-tailed
-    #z_alpha = abs(norm.ppf(alpha)) #one-tailed
+    adj_alpha = alpha / num_comparisons
+    z_alpha = abs(norm.ppf(adj_alpha/2)) #two-tailed
+    #z_alpha = abs(norm.ppf(adj_alpha)) #one-tailed
     z_beta = abs(norm.ppf(beta))
     mde = (z_alpha + z_beta) * se / pooled_prob
     return mde
 
-def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_size, num_variants):
+#Original
+#def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_size, num_variants):
+    #table = []
+    #mde_values = []
+    #total_sample_size_values = []
+    #for week in range(1, num_weeks+1):
+        #week_total_sample_size = total_sample_size * week
+        #week_sample_size = week_total_sample_size / (num_variants)
+        #mde = calculate_mde(alpha, beta, control_cr * (1 + 0.01*week), control_cr, week_sample_size, num_variants)
+        #mde = calculate_mde(alpha, beta, control_cr, control_cr, week_sample_size, num_variants)
+        #week_total_sample_size_str = format(week_total_sample_size, ".0f")
+        #week_sample_size_str = format(week_sample_size, ".0f")
+        #mde_str = f"{mde*100:.2f}%"
+        #table.append([week, week_sample_size_str, week_total_sample_size_str, mde_str])
+        #mde_values.append(mde*100)
+        #total_sample_size_values.append(week_total_sample_size)
+        
+#Bonferroni         
+def generate_table_and_plot(alpha, beta, num_weeks, control_cr, total_sample_size, num_variants, num_comparisons=1):
     table = []
     mde_values = []
     total_sample_size_values = []
     for week in range(1, num_weeks+1):
         week_total_sample_size = total_sample_size * week
         week_sample_size = week_total_sample_size / (num_variants)
-        #mde = calculate_mde(alpha, beta, control_cr * (1 + 0.01*week), control_cr, week_sample_size, num_variants)
-        mde = calculate_mde(alpha, beta, control_cr, control_cr, week_sample_size, num_variants)
+        mde = calculate_mde(alpha, beta, control_cr, control_cr, week_sample_size, num_variants, num_comparisons=num_comparisons)
         week_total_sample_size_str = format(week_total_sample_size, ".0f")
         week_sample_size_str = format(week_sample_size, ".0f")
         mde_str = f"{mde*100:.2f}%"
         table.append([week, week_sample_size_str, week_total_sample_size_str, mde_str])
         mde_values.append(mde*100)
-        total_sample_size_values.append(week_total_sample_size)
-        
+        total_sample_size_values.append(week_total_sample_size)        
+         
     sns.set_style("white")
     plt.rcParams['axes.spines.top'] = False
     plt.rcParams['axes.spines.left'] = False
